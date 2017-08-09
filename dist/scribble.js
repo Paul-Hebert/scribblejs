2a7b52294b57f81ab6fb64315a81bc171b7e1207
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2017-08-09 11:54:29 -0700

    Scribble now takes an object to overwrite default params

--- dist/scribble.js
+++ dist/scribble.js
@@ -25,36 +25,54 @@ var scribble = (function() {
         return scribbleElement;
     }
 
+    var defaults = {
+        "selector": "path, polygon",
+        "duration": "1000ms",
+        "action": "draw",
+        "loop": "false",
+        "strokeWidth": "1px",
+        "strokeColor": "#000",
+        "fillColor": "#fff"
+    };
+
+    function overwriteDefaults(customSettings){
+        var settings = defaults;
+
+        if(typeof customSettings !== "undefined"){
+            var customSettingKeys = Object.keys(customSettings);
+
+            for(var i = 0; i < customSettingKeys.length; i++){
+                settings[customSettingKeys[i]] = customSettings[customSettingKeys[i]];
+            }
+        }
+
+        return settings;
+    }
+
     return {
-        draw: function(selector, duration) {
-            this.update(selector, duration, "forwards")
-        },
+        draw: function(customSettings){
+            var settings = overwriteDefaults(customSettings);
 
-        erase: function(selector, duration) {
-            this.update(selector, duration, "backwards")
-        },
-
-        update: function(selector,duration, direction){
-            var domCollection = document.querySelectorAll(selector);
+            var domCollection = document.querySelectorAll(settings.selector);
 
             for(var i = 0; i < domCollection.length; i++){
                 var newScribbleElement = createScribbleElement(domCollection[i]);
 
                 newScribbleElement.setStyles(
                     {
-                        'fill': '#fff',
-                        'stroke': '#000',
-                        'stroke-width': '1px',
-                        'stroke-dashoffset': (direction === "forwards" ? newScribbleElement.strokeLength : "0"),
+                        'fill': settings.fillColor,
+                        'stroke': settings.strokeColor,
+                        'stroke-width': settings.strokeWidth,
+                        'stroke-dashoffset': (settings.action === "draw" ? newScribbleElement.strokeLength : "0"),
                         'stroke-dasharray': newScribbleElement.strokeLength + " " + newScribbleElement.strokeLength
                     }
                 );
 
-                newScribbleElement.setStyles({'transition': 'stroke-dashoffset ' + duration});
+                newScribbleElement.setStyles({'transition': 'stroke-dashoffset ' + settings.duration});
 
                 newScribbleElement.flushDOM();
 
-                newScribbleElement.setStyles({'stroke-dashoffset': (direction === "forwards" ? "0" : newScribbleElement.strokeLength)});                
+                newScribbleElement.setStyles({'stroke-dashoffset': (settings.action === "draw" ? "0" : newScribbleElement.strokeLength)});                
             }
         }
     }
